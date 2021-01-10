1074e82247c19e43ff7179030cd64e1c992a5f92
Theo Howell <sqonk@sqonk.com.au>
2021-01-10 11:54:20 +1100

    Chart titles removed from tests due to rendering differences in GD across systems.

--- tests/TCFuncs.php
+++ tests/TCFuncs.php
@@ -11,14 +11,16 @@ function multiLines(bool $writeToFile = false): string
     $l2 = [17,20,19,17,19,5,13,8,15,8];
      
     $plot->add('line', [$l1, $l2], [
-        'title' => 'lines',
         'xseries' => range(1, 10),
         'xformatter' => function ($v) {
             return "Pt $v";
         }
     ]);
     
-    [$img] = $plot->render(700, 500, $writeToFile); 
+    [$img] = $plot->render(700, 500, false); 
+    
+    if ($writeToFile)
+        file_put_contents('lines.png', $img);
     
     return $img;
 }
@@ -32,7 +34,6 @@ function lineFills(bool $writeToFile = false): string
     $l2 = [17,20,19,17,19,5,13,8,15,8];
      
     $plot->add('linefill', [$l2, $l1], [
-        'title' => 'line fills',
         'xseries' => range(1, 10),
         'xformatter' => function ($v) {
             return "Pt $v";
@@ -42,7 +43,10 @@ function lineFills(bool $writeToFile = false): string
         'ytitle' => 'Occurances'
     ]);
     
-    [$img] = $plot->render(700, 500, $writeToFile); 
+    [$img] = $plot->render(700, 500, false);
+    
+    if ($writeToFile)
+         file_put_contents('line_fills.png', $img);
     
     return $img;
 }
@@ -55,11 +59,12 @@ function basicScatter(bool $writeToFile = false): string
     $l1 = [19,3,2,7,9,18,4,15,12,13];
     $l2 = [17,20,19,17,19,5,13,8,15,8];
     
-    $plot->add('scatter', [$l1, $l2], [
-        'title' => 'scatter',
-    ]);
+    $plot->add('scatter', [$l1, $l2], []);
     
-    [$img] = $plot->render(700, 500, $writeToFile);
+    [$img] = $plot->render(700, 500, false);
+    
+    if ($writeToFile)
+         file_put_contents('scatter.png', $img);
     
     return $img; 
 }
@@ -73,11 +78,13 @@ function squareScatter(bool $writeToFile = false): string
     $l2 = [17,20,19,17,19,5,13,8,15,8];
     
     $plot->add('scatter', [$l1, $l2], [
-        'title' => 'square scatter',
         'scattershape' => MARK_SQUARE,
     ]);
     
-    [$img] = $plot->render(700, 500, $writeToFile);
+    [$img] = $plot->render(700, 500, false);
+    
+    if ($writeToFile)
+         file_put_contents('square_scatter.png', $img);
     
     return $img; 
 }
@@ -91,11 +98,13 @@ function scatterLine(bool $writeToFile = false): string
     $l2 = [17,20,19,17,19,5,13,8,15,8];
     
     $plot->add('scatter', [$l1, $l2], [
-        'title' => 'scatter lines',
         'scatterline' => true
     ]);
     
-    [$img] = $plot->render(700, 500, $writeToFile);
+    [$img] = $plot->render(700, 500, false);
+    
+    if ($writeToFile)
+         file_put_contents('scatter_lines.png', $img);
     
     return $img; 
 }
@@ -108,12 +117,14 @@ function scatterImpulse(bool $writeToFile = false): string
     $l1 = [19,3,2,7,9,18,4,15,12,13];
     
     $plot->add('scatter', [$l1], [
-        'title' => 'scatter impulse',
         'scatterimpulse' => true,
         'scattershape' => MARK_SQUARE
     ]);
     
-    [$img] = $plot->render(700, 500, $writeToFile);
+    [$img] = $plot->render(700, 500, false);
+    
+    if ($writeToFile)
+         file_put_contents('scatter_impulse.png', $img);
     
     return $img; 
 }
@@ -126,11 +137,12 @@ function bars(bool $writeToFile = false): string
     $l1 = [19,3,2,7,9,18,4,15,12,13];
     $l2 = [17,20,19,17,19,5,13,8,15,8];
     
-    $plot->add('bar', [$l1, $l2], [
-        'title' => 'bars',
-    ]);
+    $plot->add('bar', [$l1, $l2], []);
     
-    [$img] = $plot->render(700, 500, $writeToFile);
+    [$img] = $plot->render(700, 500, false);
+    
+    if ($writeToFile)
+         file_put_contents('bars.png', $img);
     
     return $img; 
 }
@@ -145,13 +157,15 @@ function stackedBarsAndAuxlines(bool $writeToFile = false): string
     $l3 = [2,5,13,12,17,16,7,4,17,17];
     
     $plot->add('barstacked', [$l1, $l2], [
-        'title' => 'stack bars',
         'auxlines' => [
             ['values' => $l3, 'legend' => 'auxlines']
         ]
     ]);
     
-    [$img] = $plot->render(700, 500, $writeToFile);
+    [$img] = $plot->render(700, 500, false);
+    
+    if ($writeToFile)
+         file_put_contents('stack_bars.png', $img);
     
     return $img; 
 }
@@ -165,7 +179,6 @@ function backgroundBars(bool $writeToFile = false): string
     $l2 = [17,20,19,17,19,5,13,8,15,8];
     
     $plot->add('line', [$l1], [
-        'title' => 'auxbars',
         'xseries' => range(1, 10),
         'xformatter' => function($v) {
             return "Pt $v";
@@ -178,7 +191,10 @@ function backgroundBars(bool $writeToFile = false): string
         'barColor' => 'gray',
     ]);
     
-    [$img] = $plot->render(700, 500, $writeToFile);
+    [$img] = $plot->render(700, 500, false);
+    
+    if ($writeToFile)
+         file_put_contents('auxbars.png', $img);
     
     return $img;
 }
@@ -192,7 +208,6 @@ function infiniteLines(bool $writeToFile = false): string
     $l2 = [17,20,19,17,19,5,13,8,15,8];
     
     $plot->add('line', [$l1, $l2], [
-        'title' => 'Infinite Lines',
         'xseries' => range(1, 10),
         'lines' => [
             ['direction' => 'h', 'value' => 7, 'color' => 'red'],
@@ -200,7 +215,10 @@ function infiniteLines(bool $writeToFile = false): string
         ]
     ]);
     
-    [$img] = $plot->render(700, 500, $writeToFile);
+    [$img] = $plot->render(700, 500, false);
+    
+    if ($writeToFile)
+         file_put_contents('Infinite_Lines.png', $img);
     
     return $img;
 }
@@ -214,7 +232,6 @@ function regions(bool $writeToFile = false): string
     $l2 = [17,20,19,17,19,5,13,8,15,8];
     
     $plot->add('line', [$l1], [
-        'title' => 'Regions',
         'xseries' => range(1, 10),
         'regions' => [
             ['x' => 3, 'y' => 20, 'x2' => 7, 'y2' => 15, 'color' => 'red@0.3'],
@@ -222,7 +239,10 @@ function regions(bool $writeToFile = false): string
         ]
     ]);
     
-    [$img] = $plot->render(700, 500, $writeToFile);
+    [$img] = $plot->render(700, 500, false);
+    
+    if ($writeToFile)
+         file_put_contents('regions.png', $img);
     
     return $img;
 }
@@ -234,7 +254,6 @@ function stockplot(bool $writeToFile = false): string
     
     $l1 = [3,6,2,6, 6,4,3,6, 4,6,1,8, 3,2,1,7, 2,6,2,9];
     $plot->add('stock', [$l1], [
-        'title' => 'Candlesticks',
         'xseries' => range(1, 5),
         'margin' => [55,55,55,55],
         'configCallback' => function($chart) {
@@ -244,7 +263,10 @@ function stockplot(bool $writeToFile = false): string
         }
     ]);
     
-    [$img] = $plot->render(700, 500, $writeToFile);
+    [$img] = $plot->render(700, 500, false);
+    
+    if ($writeToFile)
+         file_put_contents('candlesticks.png', $img);
     
     return $img;
 }
@@ -256,7 +278,6 @@ function boxplot(bool $writeToFile = false): string
     
     $l1 = [3,6,2,6,4, 6,4,3,6,4, 4,6,1,8,5, 3,2,1,7,4, 2,6,2,9,4];
     $plot->add('box', [$l1], [
-        'title' => 'box',
         'xseries' => range(1, 5),
         'margin' => [55,55,55,55],
         'configCallback' => function($chart) {
@@ -266,7 +287,10 @@ function boxplot(bool $writeToFile = false): string
         }
     ]);
     
-    [$img] = $plot->render(700, 500, $writeToFile);
+    [$img] = $plot->render(700, 500, false);
+    
+    if ($writeToFile)
+         file_put_contents('box.png', $img);
     
     return $img;
 }
